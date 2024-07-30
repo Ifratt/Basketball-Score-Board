f99e5b9d48c6d4d7cefec6b70c7e3de7938f1aff
Ifrat Jahan <Ifrat@Mds-MacBook-Pro.local>
2024-07-30 10:07:45 -0400

    initial commite

---
+++ index.js
@@ -0,0 +1,49 @@
+
+let count1 = 0
+let count2 = 0
+let boxEl = document.getElementById("box-1")
+let boxEl2 = document.getElementById("box-2")
+
+
+function increment1(){
+    count1 += 1
+    boxEl.innerText = count1
+}
+
+function increment2(){
+    count1 += 2
+    boxEl.innerText = count1
+}
+
+function increment3(){
+    count1 += 3
+    boxEl.innerText = count1
+}
+function reset1(){
+    count1 = 0
+    boxEl.innerText = count1
+}
+
+
+
+
+function increment4(){
+    count2 += 1
+    boxEl2.innerText = count2
+}
+
+function increment5(){
+    count2 += 2
+    boxEl2.innerText = count2
+}
+
+function increment6(){
+    count2 += 3
+    boxEl2.innerText = count2
+}
+function reset2(){
+    count2 = 0
+    boxEl2.innerText = count2
+}
+
+
